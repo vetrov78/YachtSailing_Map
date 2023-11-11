cd7fd37271f26ee5612df02a21ca83f750b7d02e
vetrov78 <vettrov78@gmail.com>
2023-11-11 11:31:10 +0300

    Использует geopy для вычисления координат

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-import requests, bs4;
+from bs4 import BeautifulSoup;
+from selenium import webdriver;
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import csv
+import re
+from geopy.geocoders import Nominatim
 
-def getDestination(liElement):
-    return liElement.find('div', 'location').contents[0];
+options = Options();
+options.add_argument('--ignore-certificate-errors')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+service = Service('C:/Users/Andrei/chromedriver-win64/chromedriver.exe')
+driver = webdriver.Chrome(service=service, options=options)
 
-url = 'https://www.yachtworld.com/';
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+url = 'https://www.boat24.com/en/secondhandboats/?src=lagoon&cat=&whr=EUR&prs_min=&prs_max=&lge_min=&lge_max=&bre_min=&bre_max=&jhr_min=&jhr_max=&sort=rand'
+driver.get(url)
 
-response = requests.get(url, headers=headers);
-print(response);
-# print(requests.get('https://httpbin.org/ip').json());
+f = open('./locations.csv', 'w+', encoding="utf-8", newline='')
+writer = csv.writer(f)
 
-# soup = bs4.BeautifulSoup(response.text, 'lxml');
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+items = soup.find_all('div', {'class': 'blurb'})
+for item in items:
+    current_row = []
 
-# list = soup.find_all('li', {'class': 'listing-result'})
+    title = item.find('h3', {'class': 'blurb__title'}).getText()
+    current_row.append(title)
 
-# for item in list:
-    # print(getDestination(item));
+    location = item.find('p', {'class': 'blurb__location'}).getText().replace('» ', '')
+    current_row.append(location)
+    geolocator = Nominatim(user_agent='VSCode')
+    geo_result = geolocator.geocode(location)
+    if geo_result : print(geo_result.latitude)
+
+    price_str = item.find('p', {'class': 'blurb__price'}).getText().replace('.', '')
+    price = int(re.search(r'\d+', price_str)[0])
+    current_row.append(price)
+
+    writer.writerow(current_row)
+
+f.close()
